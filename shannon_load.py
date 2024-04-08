e19fc73466da880d583938775b0265d2d2046700
alexander-pick <contact@alexander-pick.com>
2024-04-08 14:24:25 +0200

    some fine tuning for auto-analysis

--- shannon_load.py
+++ shannon_load.py
@@ -1,14 +1,17 @@
 #!/bin/python3
 
-#
-# samsung shannon modem loader 
-# API 2024
-#
+# Samsung Shannon Modem Loader
+# A lean IDA Pro loader for fancy baseband research 
+# Alexander Pick 2024
 
 import idaapi
+import idautils
 import ida_idp
 import idc
 import ida_auto
+import ida_bytes
+import ida_nalt
+
 import struct
 
 def accept_file(fd, fname):
@@ -32,6 +35,9 @@ def load_file(fd, neflags, format):
     idc.process_config_line("ARM_SIMPLIFY = NO")
     idc.process_config_line("ARM_NO_ARM_THUMB_SWITCH = YES")
 
+    # improve auto analysis
+    idc.process_config_line("ARM_REGTRACK_MAX_XREFS = 0")
+
     if (neflags & idaapi.NEF_RELOAD) != 0:
         return 1
 
@@ -42,6 +48,7 @@ def load_file(fd, neflags, format):
         fd.seek(start_offset)
         entry = fd.read(0x20)
 
+        #unpack TOC entry
         toc_info = struct.unpack("12sIIIII", entry)
 
         seg_name = str(toc_info[0], "UTF-8").strip("\x00")
@@ -52,7 +59,7 @@ def load_file(fd, neflags, format):
         seg_start = toc_info[2]
         seg_end = toc_info[2] + toc_info[3]
 
-        # AddSeg(startea, endea, base, use32, align, comb
+        # map slices to segments 
         idc.AddSeg(seg_start, seg_end, 0, 1, idaapi.saRel32Bytes, idaapi.scPub)
         if "NV" in seg_name:
             idc.set_segm_class(seg_start, "DATA")
@@ -62,6 +69,7 @@ def load_file(fd, neflags, format):
 
         fd.file2base(toc_info[1], seg_start, seg_end,  0)
 
+        # set entry points of main and bootloader
         if seg_name == "BOOT":
             idaapi.add_entry(seg_start, seg_end, "bootloader_entry", 1)
             ida_auto.auto_make_code(seg_start)
@@ -71,5 +79,17 @@ def load_file(fd, neflags, format):
             ida_auto.auto_make_code(seg_start)
 
         start_offset += 0x20
+
+    # pre-create long strings to avoid them beeing mistaken with code
+    # Shannon has a lot of these and IDA ocassinly eats them
+
+    strings = idautils.Strings()
+
+    strings.setup(strtypes=[ida_nalt.STRTYPE_C], ignore_instructions=True, minlen=12)
+
+    strings.refresh()
+
+    for s in strings:
+        ida_bytes.create_strlit(s.ea, 0, ida_nalt.STRTYPE_TERMCHR)
 
     return 1
